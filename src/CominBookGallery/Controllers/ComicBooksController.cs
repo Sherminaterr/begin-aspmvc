9c0d13b7b381cabed92e360888594524e4612a13
Sherman <presbyterian163@gmail.com>
2018-09-15 23:56:59 +0800

    Updated Comic Book detail view

--- src/CominBookGallery/Controllers/ComicBooksController.cs
+++ src/CominBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,20 @@ namespace CominBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            //View bag is dynamic type
+            //To follow convention, use Captial case.
+            ViewBag.SeriesTitle = "The Amaziing Spider-Man";
+            ViewBag.IssueNumber = "700";
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+        
             return View();
             //if(DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
             //{
